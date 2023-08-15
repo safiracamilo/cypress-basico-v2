a31b59f0b904098238e6239ab9bdf026c7176521
safiracamilo <safiracamilo@email.com>
2023-08-15 17:57:30 +0100

    teste

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
+
+    cy.get('#firstName').type  ('Safira') 
+    cy.get('#lastName').type ('Camilo') 
+    cy.get('#email').type  ('cypress.treinamento@gmail.com') 
+    cy.get('#phone').type('34860131') 
+    cy.get('#open-text-area').type('teste') 
+    cy.get('button[type="submit"]').click()  
+
+
+})
